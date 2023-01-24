ea516a7d5b35f2a8a25857f7444307e7573b46bb
slowlife25 <slowlife25@gmail.com>
2023-01-24 23:40:53 +0900

    fix service

--- src/app/services/firebase.analytics.service.ts
+++ src/app/services/firebase.analytics.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { Router }     from '@angular/router';
 
 import { Device }            from '@capacitor/device'
 import { FirebaseAnalytics } from '@capacitor-community/firebase-analytics'
@@ -22,29 +21,6 @@ export class FirebaseAnalyticsService {
 
   getDeviceInfo() {
     return Device.getInfo()
-  }
-
-  setUerId(userId: string) {
-    FirebaseAnalytics.setUserId({
-      userId: userId
-    })
-  }
-
-  setUserProperty(name: string, value: string): void {
-    FirebaseAnalytics.setUserProperty({
-      name: name,
-      value: value
-    })   
-  }
-
-  getAppInstanceId() {
-    return FirebaseAnalytics.getAppInstanceId()
-  }
-
-  setScreenName(router: Router): void {
-    FirebaseAnalytics.setScreenName({
-      screenName: router.url
-    })
   }
 
   logEvent(name: string, params?: Object) {
